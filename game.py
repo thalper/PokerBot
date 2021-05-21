feeaf514f3403bfd62a67b527460f622da7f80bf
Tycho <thalper@purdueedu>
2021-05-21 15:44:14 -0700

    fixed classes

--- game.py
+++ game.py
@@ -1,15 +1,13 @@
 import time
 import random
 
-hand1 = [0, 5, 22, 26, 51, 34, 50]  # 1 pair
-hand2 = [2, 15, 12, 25, 38, 0, 5]  # full house
-hand3 = [16, 18, 17, 9, 20, 35, 19]  # straight flush
-hand4 = [0, 13, 26, 5, 18, 9, 22]  # broken full house
 
-print(hand1.best)
+class Dealer:
+    def __init__(self, players):
+        self.deck = self.shuffle()
+        self.players = players
 
-class CreateDeck:
-    def shuffle():
+    def shuffle(self):
         handKey = int(round(time.time_ns()))
         handKey /= 100
         handKey %= 1000000000
@@ -31,16 +29,32 @@ class CreateDeck:
             deck.append(card)
         return deck
 
+    def deal(self):
+        hands = []
+        for i in range(self.players):
+            playerHand = []
+            playerHand.append(self.deck[i])
+            playerHand.append(self.deck[i+self.players])
+            hands.append(playerHand)
+        for i in range(self.players):
+            self.deck.pop(0)
+            self.deck.pop(0)
+        board = [self.deck[0], self.deck[1], self.deck[2], self.deck[4], self.deck[6]]
+        return [hands, board]
+
 class HandRanks:
-    def best(hand):
-        boolSF, valSF = HandRanks.SFlush(hand)
-        bool4, val4, kicker4 = HandRanks.Quads(hand)
-        boolB, valB, kickerB = HandRanks.FullHouse(hand)
-        boolF, valF = HandRanks.Flush(hand)
-        boolS, valS = HandRanks.Straight(hand)
-        bool3, val3, kicker3 = HandRanks.Trips(hand)
-        bool2, val2, kicker2 = HandRanks.TwoPair(hand)
-        boolP, valP, kickerP = HandRanks.Pair(hand)
+    def __init__(self, hand):
+        self.hand = hand
+
+    def best(self):
+        boolSF, valSF = self.SFlush()
+        bool4, val4, kicker4 = self.Quads()
+        boolB, valB, kickerB = self.FullHouse()
+        boolF, valF = self.Flush()
+        boolS, valS = self.Straight()
+        bool3, val3, kicker3 = self.Trips()
+        bool2, val2, kicker2 = self.TwoPair()
+        boolP, valP, kickerP = self.Pair()
         kicker = None
         if boolSF:
             rank = 8
@@ -74,12 +88,13 @@ class HandRanks:
         else:
             rank = 0
             val = 0 # high card
-            for x in hand:
+            for x in self.hand:
                 num = x % 13
                 if num > val:
                     val = num
-            copy = hand
-            copy.remove(val)
+                    copyx = x
+            copy = self.hand.copy()
+            copy.remove(copyx)
 
             kicker = 0 # second high card
             for x in copy:
@@ -88,72 +103,58 @@ class HandRanks:
                     kicker = num
 
         return rank, val, kicker
+
         
-    def SFlush(hand):
-        Sbool, val = HandRanks.Straight(hand)
+    def SFlush(self):
+        Sbool, val = self.Straight()
         if not Sbool:
             return False, None
         cardList = [val, val+13, val+26, val+39]
         newList = []
         for top in cardList:
-            if top in hand:
+            if top in self.hand:
                 newList.append(top)
         for card in newList:
             for i in range(5):
-                if (card-i) not in hand:
+                if (card-i) not in self.hand:
                     return False, None
         return True, val  
+
     
-    def Quads(hand):
-        Tbool, val, kicker3 = HandRanks.Trips(hand)
+    def Quads(self):
+        Tbool, val, kicker3 = self.Trips()
         if not Tbool:
             return False, None, None
-        if val in hand:
-            if val+13 in hand:
-                if val+26 in hand:
-                    if val+39 in hand:
+        if val in self.hand:
+            if val+13 in self.hand:
+                if val+26 in self.hand:
+                    if val+39 in self.hand:
                         high = 0 # kicker
-                        for x in hand:
+                        for x in self.hand:
                             num = x % 13
                             if num != val and num > high:
                                 high = num
                         return True, val, high
         return False, None, None
     
-    def FullHouse(hand):
-        Tbool, val, useless = HandRanks.Trips(hand)
+
+    def FullHouse(self):
+        Tbool, val, useless = self.Trips()
         if not Tbool:
-            return False, None
-        TwoBool, val2, kicker2 = HandRanks.TwoPair(hand)
+            return False, None, None
+        TwoBool, val2, kicker2 = self.TwoPair()
         if not TwoBool:
-            return False, None
-        if val == kicker2:
-            return True, kicker2, val2
+            return False, None, None
         if val == val2:
             return True, val, kicker2
         return True, val, val2
-        
-        
-        """
-        countVal2 = 0
-        if val2 in hand:
-            countVal2 += 1
-        if (val2+13) in hand:
-            countVal2 += 1
-        if (val2+26) in hand:
-            countVal2 += 1
-        if (val2+39) in hand:
-            countVal2 += 1
-        if countVal2 == 3 and val2 > val:
-            return True, val2, val
-"""
 
 
-    def Flush(hand):
-        board = hand[2:]
+    def Flush(self):
+        board = self.hand[2:].copy()
         suitCount = [0,0,0,0]
         boardCount = [0,0,0,0]
-        for card in hand:
+        for card in self.hand:
             suitCount[int(card/13)] += 1
         for card in board:
             boardCount[int(card/13)] += 1
@@ -163,15 +164,15 @@ class HandRanks:
         while suitCount[index] < 5:
             index += 1
         
-        card1suit = int(hand[0] / 13)
-        card2suit = int(hand[1] / 13)
+        card1suit = int(self.hand[0] / 13)
+        card2suit = int(self.hand[1] / 13)
 
         if card1suit == card2suit:
-            higher = max([hand[0], hand[1]])
+            higher = max([self.hand[0], self.hand[1]])
             if card1suit == index:
                 if max(boardCount) < 5:
                     value = higher % 13
-                elif min(board) < higher:
+                elif min(board) < higher: # fix this so it only looks at the correct suits
                     value = higher % 13
                 else:
                     value = min(board) % 13
@@ -180,17 +181,17 @@ class HandRanks:
 
         elif card1suit == index:
             if max(boardCount) < 5:
-                value = hand[0] % 13
-            elif min(board) < hand[0]:
-                value = hand[0] % 13
+                value = self.hand[0] % 13
+            elif min(board) < self.hand[0]: # fix this so it only looks at the correct suits
+                value = self.hand[0] % 13
             else:
                 value = min(board) % 13
             
         elif card2suit == index:
             if max(boardCount) < 5:
-                value = hand[1] % 13
-            elif min(board) < hand[1]:
-                value = hand[1] % 13
+                value = self.hand[1] % 13
+            elif min(board) < self.hand[1]: # fix this so it only looks at the correct suits
+                value = self.hand[1] % 13
             else:
                 value = min(board) % 13 
 
@@ -200,12 +201,12 @@ class HandRanks:
         return True, value
 
 
-    def Straight(hand):
+    def Straight(self):
         copyHand = []
-        for i in range(len(hand)):
+        for i in self.hand:
             if i % 13 not in copyHand:
                 copyHand.append(i%13)
-        copy2 = copyHand # we're going to make aces low in this copy
+        copy2 = copyHand.copy() # we're going to make aces low in this copy
         for i in range(len(copy2)):
             copy2[i] = (copy2[i] + 1) % 13
     
@@ -216,17 +217,17 @@ class HandRanks:
             copyHand.remove(high)
             
         if 0 in copy2 and 1 in copy2 and 2 in copy2 and 3 in copy2 and 4 in copy2:
-            return True, 5
+            return True, 3
 
         return False, None
 
-    def Trips(hand):
-        copyHand = hand
+    def Trips(self):
+        copyHand = self.hand.copy()
         for i in range(len(copyHand)):
-            copyHand[i] = (copyHand[i] + 1) % 13
+            copyHand[i] = (copyHand[i]) % 13
         copyHand.sort(reverse=True)
         kicker = 0
-        while len(copyHand > 2):
+        while len(copyHand) > 2:
             val = copyHand[0]
             count = 0
             for card in copyHand:
@@ -241,12 +242,12 @@ class HandRanks:
         
         return False, None, None
 
-    def TwoPair(hand):
-        copyHand = hand
+    def TwoPair(self):
+        copyHand = self.hand.copy()
         for i in range(len(copyHand)):
-            copyHand[i] = (copyHand[i] + 1) % 13
+            copyHand[i] = copyHand[i] % 13
         pairs = []
-        while len(copyHand > 1):
+        while len(copyHand) > 1:
             val = copyHand[0]
             count = 0
             for card in copyHand:
@@ -254,33 +255,60 @@ class HandRanks:
                     count += 1
             if count > 1:
                 pairs.append(val)
-            copyHand.remove(copyHand[0])
+            copyHand = [i for i in copyHand if i != val] # replace all .remove() with this
         if len(pairs) > 1:
             pairs.sort(reverse=True)
             return True, pairs[0], pairs[1]
+        return False, None, None
 
-    def Pair(hand):
-        copyHand = hand
+    def Pair(self):
+        copyHand = self.hand.copy()
         for i in range(len(copyHand)):
-            copyHand[i] = (copyHand[i] + 1) % 13
+            copyHand[i] = copyHand[i] % 13
         new = []
-        high = 0
         for card in copyHand:
             if card in new:
-                if (hand[0] % 13) == card:
-                    if (hand[1] % 13) == card:
+                if (self.hand[0] % 13) == card:
+                    if (self.hand[1] % 13) == card:
                         kicker = None
                     else:
-                        kicker = hand[1] % 13
-                elif (hand[1] % 13) == card:
-                    kicker = hand[0] % 13
+                        kicker = self.hand[1] % 13
+                elif (self.hand[1] % 13) == card:
+                    kicker = self.hand[0] % 13
                 else:
-                    kicker = max([(hand[0] % 13), (hand[1] % 13)])
+                    kicker = max([(self.hand[0] % 13), (self.hand[1] % 13)])
                 return True, card, kicker
-            copyHand.remove(card)
+            else:
+                new.append(card)
         return False, None, None
             
         
 
 ## look through all rankings and make sure kicker is high card FROM HAND if its a single card
 # fix full house if 3 pair and trips on the lowest pair 2,2,2,5,5,9,9 (example that breaks it)
+
+
+players = 2
+game = Dealer(players)
+hands = game.deal()
+board = hands[1]
+for i in range(players):
+    hand = [hands[0][0] + board]
+    hands.append(hand)
+    hands[0].pop(0)
+hands.pop(0)
+hands.pop(0)
+
+for x in range(len(hands)):
+    hands[x] = hands[x][0]
+
+
+print(hands)
+ranks = []
+for x in hands:
+    hand = HandRanks(x)
+    ranks.append(hand.best())
+
+print(ranks)
+
+#print(hand4.best())
